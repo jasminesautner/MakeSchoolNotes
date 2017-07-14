763124571f3a637d690a2f65b596a58e586cc966
Jasmine <jasmineivysautner@gmail.com>
2017-07-14 10:26:18 -0700

    Initial commit

---
+++ Controllers/DisplayNoteViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DisplayNoteViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class DisplayNoteViewController: UIViewController {
+    @IBOutlet weak var noteTitleTextField: UITextField!
+    
+    @IBOutlet weak var noteContentTextView: UITextView!
+    
+    var note: Note?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let note = note {
+            noteTitleTextField.text = note.title
+            noteContentTextView.text = note.content
+        } else {
+            noteTitleTextField.text = ""
+            noteContentTextView.text = ""
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "save" {
+            let note = self.note ?? CoreDataHelper.newNote()
+            note.title = noteTitleTextField.text ?? ""
+            note.content = noteContentTextView.text ?? ""
+            note.modificationTime = Date() as NSDate
+            CoreDataHelper.saveNote()
+        } 
+
+    }
+}
+
+
+
+
+
